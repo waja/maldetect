8b6cb288c2d9611f5548fbeee0a33d29b5f336ad
Jan Wagner <waja@cyconet.org>
2017-02-02 16:11:09 +0100

    New upstream version 1.6-rc5

--- files/hookscan.sh
+++ files/hookscan.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 file="$1"
+
 inspath='/usr/local/maldetect'
 intcnf="$inspath/internals/internals.conf"
-
 if [ -f "$intcnf" ]; then
 	source $intcnf
-else
-	header
-	echo "maldet($$): {glob} $intcnf not found, aborting."
-	exit 1
 fi
+
+## these values can be overridden in conf.maldet.hookscan
+quarantine_hits=1
+quarantine_clean=0
+scan_tmpdir_paths=''
 
 isclamd=`pidof clamd 2> /dev/null`
-
 if [ "$isclamd" ] && [ -f "$clamdscan" ]; then
 	clamd_scan=1
+else
+	clamd_scan=0
 fi
-cd /tmp ; $inspath/maldet --config-option quarantine_hits=1,quarantine_clean=0,tmpdir=/var/tmp,scan_tmpdir_paths='',scan_clamscan=$clamd_scan --hook-scan -a "$file"
+
+hookcnf="$inspath/conf.maldet.hookscan"
+if [ -f "$hookcnf" ]; then
+        source $hookcnf
+fi
+
+cd /tmp ; $inspath/maldet --config-option quarantine_hits=$quarantine_hits,quarantine_clean=$quarantine_clean,tmpdir=/var/tmp,scan_tmpdir_paths=$scan_tmpdir_paths,scan_clamscan=$clamd_scan --hook-scan -a "$file"
